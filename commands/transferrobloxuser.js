fd3caf9e6f134559f62de89aa41cf02bcb4bda3f
webbiesss <benjamincurrie09@gmail.com>
2026-08-19 00:59:47 -0700

    Update feature

--- commands/transferrobloxuser.js
+++ commands/transferrobloxuser.js
@@ -191,7 +191,10 @@ module.exports = {
                 const timeoutEmbed = new EmbedBuilder()
                     .setTitle('World Expeditions Guide Department')
                     .setDescription('Transfer confirmation timed out. Please run the command again.')
-                    .setColor(0xFF9900);
+                    .setColor(0xFF9900)
+                    .setFooter({
+                        text: `Added by ${interaction.user.username} • ${new Date().toISOString()}`
+                    });
                 return interaction.followUp({ embeds: [timeoutEmbed], ephemeral: true });
             }
         });
